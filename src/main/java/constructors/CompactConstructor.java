8b67799943b38236785c57c020df0d6b17fd3f6e
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-02 20:41:34 +0500

    .

--- src/main/java/constructors/CompactConstructor.java
+++ src/main/java/constructors/CompactConstructor.java
@@ -8,6 +8,8 @@ public record CompactConstructor(String id, int age) {
 
 class Test{
 	public static void main(String[] args) {
+		CompactConstructor compactConstructor=new CompactConstructor("2",12);
 
+		System.out.println("compactConstructor = " + compactConstructor);
 	}
 }
