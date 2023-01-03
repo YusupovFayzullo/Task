17f472ed325de86da00231225bfc67c245815c81
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-03 22:58:03 +0500

    .

--- src/main/java/epams/Task1.java
+++ src/main/java/epams/Task1.java
@@ -3,25 +3,27 @@ package epams;
 public class Task1 {
 	public static void main(String[] args) {
 
-		Task1 t1=new Task1();
-	t1.printGreatest(2,4);
-		t1.throwCoin(499);
+		Task1 t1 = new Task1();
+//	t1.printGreatest(2,4);
+//		t1.throwCoin(499);
+//
+//		t1.tellTheSeasonByMonthNumber(5);
 
-		t1.tellTheSeasonByMonthNumber(5);
+		t1.tellTheSeasonByMonthNumber(7);
 
 
 	}
 
 	public void printGreatest(int first, int second) {
 
-		System.out.println(first>second?first:second);
+		System.out.println(first > second ? first : second);
 
 	}
 
 	public void throwCoin(int from1UpTo1000) {
-		if(from1UpTo1000<500){
+		if (from1UpTo1000 < 500) {
 			System.out.println("Eagle");
-		}else {
+		} else {
 			System.out.println("Tail");
 		}
 
@@ -29,17 +31,17 @@ public class Task1 {
 
 	public void tellTheSeasonByMonthNumber(int month) {
 
-		switch (month){
-			case 12,1,2->{
+		switch (month) {
+			case 12, 1, 2 -> {
 				System.out.println("Winter");
 			}
-			case 3,4,5->{
+			case 3, 4, 5 -> {
 				System.out.println("Spring");
 			}
-			case 6,7,8->{
+			case 6, 7, 8 -> {
 				System.out.println("Summer");
 			}
-			case 9,10,11->{
+			case 9, 10, 11 -> {
 				System.out.println("Autumn");
 			}
 			default -> System.out.println("Not season in this month");
