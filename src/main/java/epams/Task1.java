117f181198e72ed5eafa9dee281c54a0755fef02
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-03 22:55:14 +0500

    .

--- src/main/java/epams/Task1.java
+++ src/main/java/epams/Task1.java
@@ -31,11 +31,18 @@ public class Task1 {
 
 		switch (month){
 			case 12,1,2->{
-				System.out.println("Winter"); break;
+				System.out.println("Winter");
 			}
 			case 3,4,5->{
-				System.out.println("Spring"); break;
+				System.out.println("Spring");
 			}
+			case 6,7,8->{
+				System.out.println("Summer");
+			}
+			case 9,10,11->{
+				System.out.println("Autumn");
+			}
+			default -> System.out.println("Not season in this month");
 		}
 
 	}
