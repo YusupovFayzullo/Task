cc6ce56b6c9eb94ed403759d9313006ff8be4ac4
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-04 14:45:19 +0500

    .

--- src/main/java/epams/Task4.java
+++ src/main/java/epams/Task4.java
@@ -2,7 +2,8 @@ package epams;
 
 public class Task4 {
 
-	public static void main(String[] args) {System.out.println("Hello world");
+	public static void main(String[] args) {
+		System.out.println("Hello world");
 	}
 }
 
