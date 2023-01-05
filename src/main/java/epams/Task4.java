cd07b257c0b359a80c8d181b9f29134a127c1c38
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-05 21:02:39 +0500

    .

--- src/main/java/epams/Task4.java
+++ src/main/java/epams/Task4.java
@@ -12,8 +12,8 @@ public class Task4 {
 		try (var fileReader = new FileReader("src/main/resources/config.properties")) {
 			prop.load(fileReader);
 			System.out.println(prop.getProperty("datasource.database.username","default.username"));
-			//  System.out.println(prop.getProperty("datasource.database.password","default.password"));
-			System.out.println(prop.getProperty("datasource.database.url","default.url"));
+			System.out.println(prop.getProperty("datasource.database.password","default.password"));
+			// System.out.println(prop.getProperty("datasource.database.url","default.url"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
