639fa9046aa1e90798bfee0565f242fe707daec1
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-05 17:02:19 +0500

    .

--- src/main/java/epams/Task4.java
+++ src/main/java/epams/Task4.java
@@ -1,9 +1,22 @@
 package epams;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Task4 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello wolr");
+
+		Properties prop=new Properties();
+		try (var fileReader = new FileReader("src/main/resources/config.properties")) {
+			prop.load(fileReader);
+			System.out.println(prop.getProperty("datasource.database.username","default.username"));
+			System.out.println(prop.getProperty("datasource.database.password","default.password"));
+			System.out.println(prop.getProperty("datasource.database.url","default.url"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
