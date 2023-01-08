84afca58aa3b47231ad63965a62cfd36389ab977
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-08 23:52:20 +0500

    .

--- src/main/java/ml/tasks/Main.java
+++ src/main/java/ml/tasks/Main.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-
-
+		Scanner scanner=new Scanner(System.in)
 		System.out.print("n=");
 		int n=scanner.nextInt();
 
